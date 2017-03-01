53d36ef30965dff60251ab824c87c27a11b525f6
AlexeyGornostaev <alexeygornostaev1@gmail.com>
2017-03-01 11:51:05 +0300

    Create API.h

---
+++ API.h
@@ -0,0 +1,30 @@
+#pragma once
+
+typedef void* Iterator;
+
+struct Sequential typedef Sequential;
+
+typedef struct Sequential {
+	
+	void* data;
+	
+	void  (*destructor) (Sequential* container);
+	
+	void (*begin) (Sequential* container, Iterator pointer);
+	void (*end) (Sequential* container, Iterator pointer);
+
+	void (*next) (Sequential* container, Iterator pointer);
+	void (*prev) (Sequential* container, Iterator pointer);
+	
+	void* (*get) (Sequential* container, Iterator pointer);	
+	void  (*insert) (Sequential* container, Iterator index, void* content);
+	void  (*resize) (Sequential* container, int size);
+	void  (*swap) (Sequential* container, Iterator index1, Iterator index2);
+
+	Iterator (*iterator_init) (struct Sequential* container);
+	void (*iterator_destroy) (Iterator pointer);
+	
+} Sequential;
+
+Sequential* vector_create (int size);
+Sequential* list_create (int size);
